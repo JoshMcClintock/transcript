c142aa61453bfbe2f34a4604b9fc25558c965b94
Bofeng <bli3@unb.ca>
2019-04-03 15:00:37 -0300

    add average grade for each transcript for each area

--- src/TranscriptAnalyzer/OutputWriter.java
+++ src/TranscriptAnalyzer/OutputWriter.java
@@ -14,7 +14,7 @@ public class OutputWriter {
 	public static void writeMasterList(int year){
 		ArrayList<String> masterList = AnalyzeTranscript.createMasterList(year);
 		try {
-			fw = new FileWriter("output/"+year+"/Master List");
+			fw = new FileWriter("output/"+year+"/Master List" + ".txt");
 			bw = new BufferedWriter(fw);
 			for (String str : masterList)
 				bw.write(str+"\n");
@@ -30,7 +30,7 @@ public class OutputWriter {
 	public static void writeDistributionPerArea(int year){
 		ArrayList<ArrayList<Integer>> distributionPerArea = AnalyzeTranscript.getGradeDistributionForEveryArea(year);
 		try {
-			fw = new FileWriter("output/"+year+"/Distribution Per Area");
+			fw = new FileWriter("output/"+year+"/Distribution Per Area" + ".txt");
 			bw = new BufferedWriter(fw);
 			writeLevelIntoFile();
 			for (int i=0;i<distributionPerArea.size();i++){
@@ -54,7 +54,7 @@ public class OutputWriter {
 	public static void writeDistributionPerCourse(int year){
 		ArrayList<ArrayList<Integer>> distributionPerCourse = AnalyzeTranscript.getGradeDistributionPerCohort(year);
 		try {
-			fw = new FileWriter("output/"+year+"/Distribution Per Course");
+			fw = new FileWriter("output/"+year+"/Distribution Per Course" + ".txt");
 			bw = new BufferedWriter(fw);
 			writeLevelIntoFile();
 			for (int i=0;i<distributionPerCourse.size();i++){
@@ -78,7 +78,7 @@ public class OutputWriter {
 	public static void writeGpaPerAreaPerTranscript(int year, int index){
 		ArrayList<Double> gpaPerAreaPerTranscript = AnalyzeTranscript.getAverageGradePerTranscriptForEachArea(year,index);
 		try{
-			fw = new FileWriter("output/"+year+"/GPA - Transcript" + index);
+			fw = new FileWriter("output/"+year+"/GPA - Transcript" + index + ".txt");
 			bw = new BufferedWriter(fw);
 			for (int i=0;i<gpaPerAreaPerTranscript.size();i++) {
 				String temp = ConfigReader.getArea().get(i).get(0);
