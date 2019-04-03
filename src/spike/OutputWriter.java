1772888f86e3f17ab9d3bae2a8df6490261a1d64
Yan <yqiu@unb.ca>
2019-04-02 22:40:21 -0300

    change the output path

--- src/spike/OutputWriter.java
+++ src/spike/OutputWriter.java
@@ -11,7 +11,7 @@ public class OutputWriter {
 	public static void writeMasterList(int year){
 		ArrayList<String> masterlist = AnalyzeTranscript.createMasterList(year);
 		try {
-			fw = new FileWriter("output/MasterList-" + year);
+			fw = new FileWriter("output/"+year+"/Master List");
 			bw = new BufferedWriter(fw);
 			for (String str : masterlist)
 				bw.write(str+"\n");
@@ -27,7 +27,7 @@ public class OutputWriter {
 	public static void writeDistributionPerArea(int year){
 		ArrayList<ArrayList<Integer>> distributionPerArea = AnalyzeTranscript.getGradeDistributionForEveryArea(year);
 		try {
-			fw = new FileWriter("output/distribution_per_area-" + year);
+			fw = new FileWriter("output/"+year+"/Distribution Per Area");
 			bw = new BufferedWriter(fw);
 			for (int i=0;i<ConfigReader.getLevel().get(0).size();i++){
 				bw.write(ConfigReader.getLevel().get(i).get(0)+"\t");
@@ -52,7 +52,7 @@ public class OutputWriter {
 	public static void writeDistributionPerCourse(int year){
 		ArrayList<ArrayList<Integer>> distributionPerCourse = AnalyzeTranscript.getGradeDistributionPerCohort(year);
 		try {
-			fw = new FileWriter("output/distribution_per_course-" + year);
+			fw = new FileWriter("output/"+year+"/Distribution Per Course");
 			bw = new BufferedWriter(fw);
 			for (int i=0;i<ConfigReader.getLevel().get(0).size();i++){
 				bw.write(ConfigReader.getLevel().get(i).get(0)+"\t");
